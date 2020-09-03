f1ef67ea754621d1b08ea716a58f58ee502c4855
joyinmyzone <jxyzsyxx17@hotmail.com>
2020-09-03 14:24:53 -0700

    Flutter Codelab

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp()); // one line method => (like a lambda function)
+
+// extends StatelessWidget, which makes the app itself a widget
+// in Flutter, almost everything is a widget
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: Text(wordPair.asPascalCase),
+        ),
+      ),
+    );
+  }
+}
+
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
